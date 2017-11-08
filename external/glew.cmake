11175788152a39c4ed63806cfb1b72eb08921fa1
Patrick Purcell <p.84@live.com>
2017-11-08 12:38:43 -0800

    Adding GLEW

---
+++ external/glew.cmake
@@ -0,0 +1,7 @@
+
+# CMake configuration for external/glew
+
+if (MSVC)
+    set(glew.INCLUDE "${CMAKE_CURRENT_SOURCE_DIR}/external/glew-2.1.0/include/")
+    set(glew.LIBRARY "${CMAKE_CURRENT_SOURCE_DIR}/external/glew-2.1.0/lib/Release/x64/glew32s.lib")
+endif()
