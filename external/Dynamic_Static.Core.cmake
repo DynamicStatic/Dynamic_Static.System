faa4e0efba8c500870c74328f845c71c79f8f49e
Patrick Purcell <p.84@live.com>
2017-11-17 07:38:11 -0800

    Debugging build

--- external/Dynamic_Static.Core.cmake
+++ external/Dynamic_Static.Core.cmake
@@ -11,9 +11,6 @@ ExternalProject_Add(
     INSTALL_COMMAND ""
 )
 
-# NOTE : We're setting up the INCLUDE and LIBRARY variables for dst::core because without the INCLUDE
-#        it won't get added to the build target's INCLUDE_DIRECTORIES property...super annoying.
-# NOTE : There's a comment with some more detail in source/CMakeLists.txt.
 ExternalProject_Get_Property(Dynamic_Static.Core.package BINARY_DIR)
 set(Dynamic_Static.Core.configuration ${BINARY_DIR}/Dynamic_Static.Core.package.cmake)
 if (EXISTS ${Dynamic_Static.Core.configuration})
@@ -24,12 +21,3 @@ if (EXISTS ${Dynamic_Static.Core.configuration})
         INTERFACE_INCLUDE_DIRECTORIES
     )
 endif()
-
-# ExternalProject_Get_Property(Dynamic_Static.Core.package SOURCE_DIR)
-# ExternalProject_Get_Property(Dynamic_Static.Core.package BINARY_DIR)
-# set(Dynamic_Static.Core.INCLUDE "${SOURCE_DIR}/include/")
-# if (MSVC)
-#     set(Dynamic_Static.Core.LIBRARY "${BINARY_DIR}/source/$(Configuration)/Dynamic_Static.Core.lib")
-# else()
-#     set(Dynamic_Static.Core.LIBRARY "${BINARY_DIR}/source/Dynamic_Static.Core.a")
-# endif()
