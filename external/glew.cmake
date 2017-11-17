8c91b3dc910cdc07e1cb92591a49087929da0663
Patrick Purcell <p.84@live.com>
2017-11-17 06:46:31 -0800

    Debugging build

--- external/glew.cmake
+++ external/glew.cmake
@@ -1,5 +1,5 @@
 
 if (MSVC)
-    set(glew.include "${CMAKE_CURRENT_SOURCE_DIR}/external/glew-2.1.0/include/")
-    set(glew.library "${CMAKE_CURRENT_SOURCE_DIR}/external/glew-2.1.0/lib/Release/x64/glew32s.lib")
+    set(glew.include "${PROJECT_SOURCE_DIR}/external/glew-2.1.0/include/")
+    set(glew.library "${PROJECT_SOURCE_DIR}/external/glew-2.1.0/lib/Release/x64/glew32s.lib")
 endif()
