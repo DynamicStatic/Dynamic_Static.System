1f4d6a017758814847fae613b1f84611408f9e04
Patrick Purcell <p.84@live.com>
2017-11-19 15:53:04 -0800

    Setting up build system

--- external/Dynamic_Static.Core.cmake
+++ external/Dynamic_Static.Core.cmake
@@ -1,26 +1,12 @@
 
+cmake_minimum_required(VERSION 3.3)
 include(ExternalProject)
 ExternalProject_Add(
-    Dynamic_Static.Core.package
-    PREFIX external
-    DOWNLOAD_DIR external/Dynamic_Static.Core
+    Dynamic_Static.Core
     URL https://github.com/DynamicStatic/Dynamic_Static.Core/archive/master.zip
+    BINARY_DIR build
     CMAKE_ARGS
         -DBUILD_SANDBOX=OFF
         -DBUILD_TESTS=OFF
     INSTALL_COMMAND ""
 )
-
-set(Dynamic_Static.Core.ready FALSE)
-ExternalProject_Get_Property(Dynamic_Static.Core.package BINARY_DIR)
-set(Dynamic_Static.Core.configuration ${BINARY_DIR}/Dynamic_Static.Core.package.cmake)
-if (EXISTS ${Dynamic_Static.Core.configuration})
-    include(${Dynamic_Static.Core.configuration})
-    get_target_property(
-        Dynamic_Static.Core.includeDirectories
-        Dynamic_Static.Core
-        INTERFACE_INCLUDE_DIRECTORIES
-    )
-
-    set(Dynamic_Static.Core.ready TRUE)
-endif()
--- external/glfw.cmake
+++ external/glfw.cmake
@@ -4,12 +4,8 @@ ExternalProject_Add(
     glfw
     PREFIX external
     DOWNLOAD_DIR external/glfw
-    # URL "${CMAKE_SOURCE_DIR}/external/glfw-3.2.1.zip"
-    # URL_MD5 35A0672885AD4CA59C8EBE7550176276
     URL https://github.com/glfw/glfw/archive/3.2.1.zip
     URL_HASH SHA1=3578BEFA2B96A6A2286F33C3A92E08E9F2F834F8
-    # GIT_REPOSITORY https://github.com/glfw/glfw.git
-    # GIT_TAG 3.2.1
     CMAKE_ARGS
         -DGLFW_BUILD_EXAMPLES=OFF
         -DGLFW_BUILD_TESTS=OFF
